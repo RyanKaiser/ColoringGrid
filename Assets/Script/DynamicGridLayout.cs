a9483f20692732e29173492da04a58519fe51bfa
Ryan <ryanhyukkim@gmail.com>
2024-11-18 15:17:36 -0800

    dynamic grid layout on palette

---
+++ Assets/Script/DynamicGridLayout.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class DynamicGridLayout : MonoBehaviour
+{
+    [SerializeField] private GridLayoutGroup _gridLayoutGroup;
+    [SerializeField] private RectTransform _parentRectTransform;
+
+    void OnRectTransformDimensionsChange()
+    {
+        AdjustCellSize();
+    }
+
+    void OnValidate()
+    {
+        if (_gridLayoutGroup != null && _parentRectTransform != null)
+        {
+            AdjustCellSize();
+        }
+    }
+
+    void AdjustCellSize()
+    {
+        float parentWidth = _parentRectTransform.rect.width;
+        float spacing = _gridLayoutGroup.spacing.x;
+        float padding = _gridLayoutGroup.padding.left + _gridLayoutGroup.padding.right;
+
+        Debug.Log($"width: {parentWidth}, spacing: {spacing}, padding: {padding}");
+
+        float cellWidth = (parentWidth - padding - spacing) / 2;
+
+        _gridLayoutGroup.cellSize = new Vector2(cellWidth, cellWidth);
+    }
+}
